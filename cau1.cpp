ae59515bccf1df194008dcecc5cfeaff6d190182
tran quoc kha <tranquockha1202203@gmail.com>
2024-08-07 01:08:54 +0700

    baitap_buoi4

---
+++ cau1.cpp
@@ -0,0 +1,30 @@
+﻿//#include <stdio.h>
+//#include <ctype.h>
+//
+//int main() {
+//    char s[100];
+//    int i, isDigit = 1;
+//
+//    printf("Nhập chuỗi: ");
+//    fgets(s, sizeof(s), stdin);
+//
+//    for (i = 0; s[i] != '\0'; i++) {
+//        if (s[i] == '\n') {
+//            s[i] = '\0';
+//            break;
+//        }
+//        if (!isdigit(s[i])) {
+//            isDigit = 0;
+//            break;
+//        }
+//    }
+//
+//    if (isDigit) {
+//        printf("Chuỗi chỉ chứa ký số.\n");
+//    }
+//    else {
+//        printf("Chuỗi không chỉ chứa ký số.\n");
+//    }
+//
+//    return 0;
+//}
